faec661231f5d05f918aebdbe88fc6290b3c70e7
FeddericoWayne <jackiew1120@hotmail.com>
2022-11-19 14:01:25 -0500

    updates

--- Develop/script.js
+++ Develop/script.js
@@ -13,11 +13,12 @@ var shuffledString = [];
 
 
 /* Alerting users of the purpose of the App */
-alert('Press OK to Start Creating your Custom Password!');
+alert('Press OK to start creating your custom password!');
 
 /* Asking user how many digits are needed for the random password */
 var digitCount = function() {
-  return Number(window.prompt("Please enter the number of characters you'd like your new password to be - at least 8 characters and no more than 128 characters!", "Enter a number between 8 and 128"));
+  return Number(window.prompt("Please enter the number of characters you'd like your new password to be:", "Enter a number between 8 and 128"));
+
   
 };
 
@@ -46,59 +47,59 @@ var typeSelection = function() {
 //AB
   } else if (includeSpecialCharacters && includeNumbers && !includeUpperCase && !includeLowerCase) {
   concatArray = originalArray.concat(specialCharacters, numerics);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //AC
   } else if (includeSpecialCharacters && !includeNumbers && includeUpperCase && !includeLowerCase) {
   concatArray = originalArray.concat(specialCharacters,upperCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //AD
   } else if (includeSpecialCharacters && !includeNumbers && !includeUpperCase && includeLowerCase) {
   concatArray = originalArray.concat(specialCharacters,lowerCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //ABC  
   } else if (includeSpecialCharacters && includeNumbers && includeUpperCase && !includeLowerCase) {
   concatArray = originalArray.concat(specialCharacters,numerics,upperCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //ABD
   } else if (includeSpecialCharacters && includeNumbers && !includeUpperCase && includeLowerCase) {
   concatArray = originalArray.concat(specialCharacters,numerics,lowerCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //ACD
   } else if (includeSpecialCharacters && !includeNumbers && includeUpperCase && includeLowerCase) {
   concatArray = originalArray.concat(specialCharacters,upperCaseLetters,lowerCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //ABCD
   } else if (includeSpecialCharacters && includeNumbers && includeUpperCase && includeLowerCase) {
   concatArray = originalArray.concat(specialCharacters,numerics,upperCaseLetters,lowerCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //B
   } else if (!includeSpecialCharacters && includeNumbers && !includeUpperCase && !includeLowerCase) {
   concatArray = originalArray.concat(numerics);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //BC
   } else if (!includeSpecialCharacters && includeNumbers && includeUpperCase && !includeLowerCase) {
   concatArray = originalArray.concat(numerics,upperCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //BD
   } else if (!includeSpecialCharacters && includeNumbers && !includeUpperCase && includeLowerCase) {
   concatArray = originalArray.concat(numerics,lowerCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //BCD
   } else if (!includeSpecialCharacters && includeNumbers && includeUpperCase && includeLowerCase) {
   concatArray = originalArray.concat(numerics,upperCaseLetters,lowerCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //C
   } else if (!includeSpecialCharacters && !includeNumbers && includeUpperCase && !includeLowerCase) {
   concatArray = originalArray.concat(upperCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //CD
   } else if (!includeSpecialCharacters && !includeNumbers && includeUpperCase && includeLowerCase) {
   concatArray = originalArray.concat(upperCaseLetters,lowerCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //D
   } else if (!includeSpecialCharacters && !includeNumbers && !includeUpperCase && includeLowerCase) {
   concatArray = originalArray.concat(lowerCaseLetters);
-  alert("Click on the Red Button to Display your New Password!");
+  alert("Click on the red button to display your new password!");
   //None Selected
   } else {
   alert("Please select at least one type of characters for your new password.");
